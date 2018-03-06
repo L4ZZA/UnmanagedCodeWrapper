faf67678d9e28cf966a087657d2ef5c55acda4fd
Pietro Lazzarotto <pietro@green-hippo.com>
2018-03-06 18:08:18 +0000

    Added console application project

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CLIWrapper;
+
+namespace ConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity e = new Entity("The Wallman", 20, 35);
+            e.Move(5, -10);
+            Console.WriteLine(e.XPosition + " " + e.YPosition);
+            Console.Read();
+        }
+    }
+}
